f2add8b3f715e537fd794f850993c342ffce1b3b
pedramaghasian <pedram.aghasian@yahoo.com>
2024-01-28 12:11:15 +0330

    feat: versioning

---
+++ src/orders/base-aggregate.ts
@@ -0,0 +1,21 @@
+import { AggregateRoot } from '@nestjs/cqrs';
+import { IMetadata } from './interfaces';
+
+export class BaseAggregate extends AggregateRoot {
+    protected id: string;
+
+    protected versionHistory: Record<number, IMetadata> = {};
+
+    getVersionedMeta(meta: IMetadata): IMetadata {
+        return {
+            ...meta,
+            version:
+                Math.max(
+                    0,
+                    ...Object.keys(this.versionHistory).map((k) => parseInt(k, 10)),
+                ) + 1,
+            timestamp: new Date().getTime(),
+        };
+
+    }
+}
--- src/orders/order.model.ts
+++ src/orders/order.model.ts
@@ -1,11 +1,11 @@
-import { AggregateRoot } from '@nestjs/cqrs';
 import { CreateOrderDto, UpdateOrderDto } from './dtos';
 import { OrderCreatedEvent } from './events/impl/order-created.event';
 import { IMetadata } from './interfaces';
 import { v4 as uuidv4 } from 'uuid';
 import { OrderUpdatedEvent } from './events/impl';
+import { BaseAggregate } from './base-aggregate';
 
-export class Order extends AggregateRoot {
+export class Order extends  BaseAggregate {
 
     public id: string;
 
@@ -16,23 +16,23 @@ export class Order extends AggregateRoot {
         if (this.id) {
             throw new Error(`Duplication Id: ${this.id}`);
         }
-        this.apply(new OrderCreatedEvent({ ...data, id: uuidv4() }, { ...meta, eventId: uuidv4() }));
+        this.apply(new OrderCreatedEvent({ ...data, id: uuidv4() }, this.getVersionedMeta({ ...meta, eventId: uuidv4() })));
     }
 
     onOrderCreatedEvent(event: OrderCreatedEvent) {
         this.id = event.data.id;
         this.name = event.data.name;
+        this.versionHistory[event.meta.version] = event.meta;
     }
 
     update(data: UpdateOrderDto, meta: IMetadata) {
-        this.apply(new OrderUpdatedEvent({ ...data }, { ...meta, eventId: uuidv4() }));
+        this.apply(new OrderUpdatedEvent({ ...data }, this.getVersionedMeta({ ...meta, eventId: uuidv4() })));
     }
 
     onOrderUpdatedEventt(event: OrderUpdatedEvent) {
         this.id = event.data.id
         this.name = event.data.name;
+        this.versionHistory[event.meta.version] = event.meta;
     }
-
-
 }
 
